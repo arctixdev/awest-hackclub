f66b7dc15339101ba206878dfa5e857e9f826822
Jonathan Bangert <jonathan@bangert.dk>
2024-10-02 14:26:10 -0600

    make basic stuff work

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,16 +1,33 @@
 'use client';
 
-import { Sun } from "lucide-react";
+import { Sun, Moon } from "lucide-react";
 import { Toggle } from "./ui/toggle";
+import { useState } from "react";
+import { useTheme } from "next-themes";
 
 export default function ModeToggle() {
+    const [mode, setMode] = useState<"light" | "dark">("light");
+    const { setTheme } = useTheme();
+
+    function changeTheme() {
+        if (mode === "light") {
+            console.log("dark");
+            setTheme("dark");
+            setMode("dark");
+        } else {
+            console.log("light");
+            setTheme("light");
+            setMode("light");
+        }
+    }
+
     return (
         <Toggle
-        aria-label="Toggle dark mode"
-        className="rounded-full"
-        onChange={() => {}}
+            aria-label="Toggle dark mode"
+            className="rounded-full"
+            onPressedChange={changeTheme}
         >
-            <Sun className="size-4" />
+            {mode === "light" ? <Sun className="size-4" /> : <Moon className="size-4" />}
         </Toggle>
     );
 }
